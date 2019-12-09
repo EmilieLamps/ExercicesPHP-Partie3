fcee658cb14c8e19300064de2784f387b0cf66d1
EmilieLamps <emilie.lamps@gmail.com>
2019-12-09 15:00:40 +0100

    Fin de l'exercice

--- Ex6/index.php
+++ Ex6/index.php
@@ -1,14 +1,16 @@
 <?php
-$page = "Exercice "; // Définir la variable pour changer le titre !
-
+$page = "Exercice 6"; // Définir la variable pour changer le titre !
+$number = 20;
 include '../header.php';
  ?>
- <h2 class="text-primary text-center pt-5 pb-5 font-weight-bold">Exercice </h2>
- <h3 class="text-center pb-5"></h3>
+ <h2 class="text-primary text-center pt-5 pb-5 font-weight-bold">Exercice 6</h2>
+ <h3 class="text-center pb-5">En allant de 20 à 0 avec un pas de 1, afficher le message C'est presque bon.</h3>
 <?php
-
+for ($number=20; $number >0 ; --$number) {
+  echo 'C\'est presque bon <br/>';
+}
 ?>
-<p><</p>
+<p><?= $number ?></p>
 <?php
 include '../footer.php';
 ?>
