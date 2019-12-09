d9b5001d5cf67bd67e427f369fee4e3752b1db75
EmilieLamps <emilie.lamps@gmail.com>
2019-12-09 10:06:45 +0100

    Fin de l'exercice

--- Ex2/index.php
+++ Ex2/index.php
@@ -1,14 +1,29 @@
 <?php
-$page = "Exercice "; // Définir la variable pour changer le titre !
-
+$page = "Exercice 2"; // Définir la variable pour changer le titre !
+$firstVar = 0;
+$secondVar = 1;
+$multiplyResult = '';
+$message = '';
 include '../header.php';
  ?>
- <h2 class="text-primary text-center pt-5 pb-5 font-weight-bold">Exercice </h2>
- <h3 class="text-center pb-5"></h3>
-<?php
+ <h2 class="text-primary text-center pt-5 pb-5 font-weight-bold">Exercice 2</h2>
+ <h3 class="text-center pb-5">Créer deux variables. Initialiser la première à 0 et la deuxième avec un nombre compris en 1 et 100.
+Tant que la première variable n'est pas supérieure à 20 :
 
+    multiplier la première variable avec la deuxième
+    afficher le résultat
+    incrementer la première variable
+</h3>
+<?php
+// utiliser la boucle do/while
+  do {
+    $multiplyResult = $firstVar * $secondVar;
+    echo $multiplyResult;
+    $message = $multiplyResult;
+    $firstVar++;
+  } while ($firstVar <= 20);
 ?>
-<p><</p>
+<p>La première variable contient <?= $message ?></p>
 <?php
 include '../footer.php';
 ?>
