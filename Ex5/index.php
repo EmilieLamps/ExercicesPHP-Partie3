e908650b0b9345c42164d66d5797338dcb1343ec
EmilieLamps <emilie.lamps@gmail.com>
2019-12-09 14:49:43 +0100

    Fin de l'exercice

--- Ex5/index.php
+++ Ex5/index.php
@@ -1,14 +1,17 @@
 <?php
-$page = "Exercice "; // Définir la variable pour changer le titre !
-
+$page = "Exercice 5"; // Définir la variable pour changer le titre !
+$number = 1;
+$message = '';
 include '../header.php';
  ?>
- <h2 class="text-primary text-center pt-5 pb-5 font-weight-bold">Exercice </h2>
- <h3 class="text-center pb-5"></h3>
+ <h2 class="text-primary text-center pt-5 pb-5 font-weight-bold">Exercice 5</h2>
+ <h3 class="text-center pb-5">En allant de 1 à 15 avec un pas de 1, afficher le message On y arrive presque.</h3>
 <?php
-
+  for ($number=1; $number <15 ; $number++) {
+    echo 'On y arrive presque <br/>';
+  }
 ?>
-<p><</p>
+<p><?= $number ?></p>
 <?php
 include '../footer.php';
 ?>
