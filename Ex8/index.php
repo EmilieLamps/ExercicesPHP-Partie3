8bf30868b9258d91258d2ea8359b4c35ffb8036e
EmilieLamps <emilie.lamps@gmail.com>
2019-12-09 15:13:19 +0100

    Fin de l'exercice

--- Ex8/index.php
+++ Ex8/index.php
@@ -1,14 +1,15 @@
 <?php
-$page = "Exercice "; // Définir la variable pour changer le titre !
-
+$page = "Exercice 8"; // Définir la variable pour changer le titre !
+$number = 200;
+$message = '';
 include '../header.php';
+for ($number=200; $number >0 ; --$number) {
+  $message = 'Enfin !!!!';
+}
  ?>
- <h2 class="text-primary text-center pt-5 pb-5 font-weight-bold">Exercice </h2>
- <h3 class="text-center pb-5"></h3>
-<?php
-
-?>
-<p><</p>
+ <h2 class="text-primary text-center pt-5 pb-5 font-weight-bold">Exercice 8</h2>
+ <h3 class="text-center pb-5">En allant de 200 à 0 avec un pas de 12, afficher le message Enfin !!!!.</h3>
+<p><?= $message ?></p>
 <?php
 include '../footer.php';
 ?>
